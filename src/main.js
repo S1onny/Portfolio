607b8cb662774e67987215a8888e73abb4fc342e
Sonny <kasdevia@gmail.com>
2026-04-07 20:11:23 +0400

    Working with plugins and animations

--- src/main.js
+++ src/main.js
@@ -1,2 +1,57 @@
 import "./styles/main.css";
 console.log("Hello Vite + Tailwind");
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+
+// init Swiper:
+// const swiper = new Swiper(".swiper", {
+//   // Optional parameters
+//   direction: "vertical",
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+import Lenis from "lenis";
+
+const lenis = new Lenis({
+  duration: 1.65, // плавность
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  smoothWheel: true,
+  smoothTouch: true,
+  wheelMultiplier: 1,
+  touchMultiplier: 2,
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+requestAnimationFrame(raf);
+
+console.log("✅ Lenis успешно запущен");
+
+// Плавный переход по ссылкам в навбаре
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", (e) => {
+    e.preventDefault();
+    const target = document.querySelector(anchor.getAttribute("href"));
+    if (target) {
+      lenis.scrollTo(target, {
+        offset: -90,
+        duration: 2.0,
+      });
+    }
+  });
+});
